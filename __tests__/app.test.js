c97d11220f51ac216dc030744acc587188b97873
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-08-13 17:15:03 -0700

    fixed test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,22 +24,14 @@ describe('app routes', () => {
 
     const expectation = [
       {
-        'id': 1,
-        'name': 'bessie',
-        'coolfactor': 3,
-        'owner_id': 1
+        
+       
       },
       {
-        'id': 2,
-        'name': 'jumpy',
-        'coolfactor': 4,
-        'owner_id': 1
+        
       },
       {
-        'id': 3,
-        'name': 'spot',
-        'coolfactor': 10,
-        'owner_id': 1
+        
       }
     ];
 
