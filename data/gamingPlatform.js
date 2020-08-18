9b8d51f93d66856312637d7a4d8409685dd7fa8b
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-08-17 17:08:10 -0700

    commit

--- data/gamingPlatform.js
+++ data/gamingPlatform.js
@@ -1,18 +1,14 @@
 module.exports = [
   {
-    id: '1',
     name: 'Xbox One'
   },
   {
-    id: '2',
     name: 'PS4'
   },
   {
-    id: 3,
     name: 'PC'
   },
   {
-    id: 4,
     name: 'Nintendo'
   }
 ];
